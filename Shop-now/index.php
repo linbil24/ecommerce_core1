574251831da1a6a451d7dd2f4f91c06327ba2817
Celestial24 <email_ng_celestial24@gmail.com>
2026-01-23 11:06:53 +0800

    .

--- Shop-now/index.php
+++ Shop-now/index.php
@@ -363,7 +363,7 @@
                         <!-- Item 1 -->
                         <div class="featured-card">
                             <a href="?store=UrbanWear+PH" style="display:block; width:100%;">
-                                <img src="../image/shop/UrbanWear PH/H&M Loose Fit Hoodie.jpeg" alt="H&M Hoodie"
+                                <img src="../image/Shop/UrbanWear PH/H&M Loose Fit Hoodie.jpeg" alt="H&M Hoodie"
                                     style="width: 100%; height: 350px; object-fit: cover; object-position: top; border-radius: 0; margin-bottom: 0;">
                             </a>
                         </div>
@@ -371,7 +371,7 @@
                         <!-- Item 2 -->
                         <div class="featured-card">
                             <a href="?store=UrbanWear+PH" style="display:block; width:100%;">
-                                <img src="../image/shop/UrbanWear PH/Pilipinas Hoodie.avif" alt="Pilipinas Hoodie"
+                                <img src="../image/Shop/UrbanWear PH/Pilipinas Hoodie.avif" alt="Pilipinas Hoodie"
                                     style="width: 100%; height: 350px; object-fit: cover; object-position: center; border-radius: 0; margin-bottom: 0;">
                             </a>
                         </div>
@@ -379,7 +379,7 @@
                         <!-- Item 3 -->
                         <div class="featured-card">
                             <a href="?store=UrbanWear+PH" style="display:block; width:100%;">
-                                <img src="../image/shop/UrbanWear PH/Team SKOOP Denim Jacket.jpeg" alt="Team SKOOP"
+                                <img src="../image/Shop/UrbanWear PH/Team SKOOP Denim Jacket.jpeg" alt="Team SKOOP"
                                     style="width: 100%; height: 350px; object-fit: cover; object-position: top; border-radius: 0; margin-bottom: 0;">
                             </a>
                         </div>
@@ -577,5 +577,3 @@
 </body>
 
 </html>
-
-
