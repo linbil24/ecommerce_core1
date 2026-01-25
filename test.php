ebe65729bcd7c6d6ca815b35b3a07d2211ba5e09
Celestial24 <email_ng_celestial24@gmail.com>
2026-01-25 11:16:18 +0800

    CRITICAL FIX: Clean index.html with no regex errors and correct paths

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Server Test</title>
+</head>
+
+<body>
+    <h1>Server is working!</h1>
+    <p>Time:
+        <?php echo date('Y-m-d H:i:s'); ?>
+    </p>
+    <p>Image test:</p>
+    <img src="image/logo.png" alt="Logo" style="max-width: 200px;">
+    <p>If you see the logo above, the path is correct.</p>
+    <p><a href="php/login.php">Go to Login</a></p>
+</body>
+
+</html>
